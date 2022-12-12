7d083163991c392d20ace216b8576ba6782075e7
Vladimir83 <vovaf@bk.ru>
2022-12-12 13:38:12 +0300

    task23

---
+++ task23/Program.cs
@@ -0,0 +1,22 @@
+﻿void Tabl(int n)
+{
+    int count = 1;
+    while (count <= n)
+        {
+            if (count == n)
+            {
+                double kub = Math.Pow(count,3);
+                Console.Write($"{kub}");
+                Console.WriteLine();
+            }
+            else
+            {
+                double kub = Math.Pow(count,3);
+                Console.Write($"{kub}, ");
+            }
+
+            count +=1;
+        }
+}
+Console.WriteLine("Введите число:");
+Tabl(int.Parse(Console.ReadLine() ?? ""));
