67fb503b13d28107de56febfbe8cc005c963697e
Vladimir83 <vovaf@bk.ru>
2022-12-12 13:27:07 +0300

    task19

---
+++ task19/Program.cs
@@ -0,0 +1,23 @@
+﻿string Polindrom(string str)
+{
+    int n = str.Length;
+    int count = 0;
+    int half = n / 2;
+    while (count < half)
+    {
+        if (str[count] == str[n-1])
+        {
+            n--; 
+            count++;      
+        }  
+        else
+            break;
+    }
+    if (half == count)
+        return "Полиндром";
+    else
+        return "Не полиндром";
+}
+
+Console.WriteLine("Bведите число:");
+Console.WriteLine(Polindrom(Console.ReadLine() ?? ""));
